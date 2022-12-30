4ebc289a0ffb14d8fdbd58a34ebd42b8cebcc898
iammurad <murad.theynalli@gmail.com>
2022-12-30 16:42:27 +0100

    add fonts

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        gemunu : ['Gemunu Libre','sans-serif'],
+        open:['Open Sans','sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
